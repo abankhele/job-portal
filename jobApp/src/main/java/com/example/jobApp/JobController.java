79c430b12b660b45d2d666dde346c5a2b43ce603
Akanksha Bankhele <bankheleakanksha@gmail.com>
2025-02-28 22:48:02 -0500

    repo added

--- jobApp/src/main/java/com/example/jobApp/JobController.java
+++ jobApp/src/main/java/com/example/jobApp/JobController.java
@@ -1,2 +1,31 @@
-package com.example.jobApp;public class JobController {
+package com.example.jobApp;
+
+import com.example.jobApp.model.JobPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JobController {
+    /*
+    We replaced @Requestmapping with Get and post because by default it is only get and we need to add data and not only fetch
+     */
+
+    @GetMapping({"/", "home"})
+    public String Home(){
+        return "home";
+    }
+    @GetMapping("addjob")
+    public String addJob(){
+
+        return "addjob";
+
+    }
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost){
+
+        return "success";
+    }
+
 }
